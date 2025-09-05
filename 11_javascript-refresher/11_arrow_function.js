f568cb3a04a1f30df27e738730c238b145144a16
aljon <aljonkenfernandez36@gmail.com>
2025-09-05 20:33:54 +0800

    Add arrow functions activity

---
+++ 11_javascript-refresher/11_arrow_function.js
@@ -0,0 +1,9 @@
+const greet = (name) => {console.log(`Hello ${name}`)}
+
+const square = (n) => {return (console.log(n*n))}
+
+const sayHi = () => console.log("Hi")
+
+greet("aljon")
+square(4)
+sayHi()
