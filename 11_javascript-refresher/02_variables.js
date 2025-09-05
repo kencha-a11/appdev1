28ae0029cc87d27712bece3bf7b04678c81ad537
aljon <aljonkenfernandez36@gmail.com>
2025-09-05 20:04:27 +0800

    Add variables activity

---
+++ 11_javascript-refresher/02_variables.js
@@ -0,0 +1,21 @@
+let name = 'aljon'
+let age = 20
+let isStudent = true
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isStudent)
+
+let numberA = 6
+let numberB = 3
+
+console.log(numberA + numberB)
+console.log(numberA - numberB)
+console.log(numberA * numberB)
+console.log(numberA / numberB)
+
+console.log("5" == 5)
+console.log("5" === 5)
+
+// double equal will convert both in same type and compare
+// triple equal or strictly equal is identity comparison
