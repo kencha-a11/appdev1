e55da9aca3f4522275979394cd6496ee56dbc647
aljon <aljonkenfernandez36@gmail.com>
2025-09-14 00:01:15 +0800

    Done displaying data

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -6,13 +6,38 @@ function MyButton() {
   );
 }
 
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+    </>
+  );
+}
+
+
 
 function App() {
 
   return (
     <>
-    <h1 style={{color: 'blue'}}>App Component</h1>
+      <h1 style={{ color: 'blue' }}>App Component</h1>
       <MyButton />
+      <Profile />
     </>
   )
 }
