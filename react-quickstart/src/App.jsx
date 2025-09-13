d87c9afde28f482db9be902fbb20e86e62a2fe05
aljon <aljonkenfernandez36@gmail.com>
2025-09-14 00:09:50 +0800

    Done rendering lists

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,16 +1,22 @@
 import './App.css'
 
-function MyButton() {
-  return (
-    <button className='red-btn'>I'm a button</button>
-  );
-}
-
 const user = {
   name: 'Hedy Lamarr',
   imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
   imageSize: 90,
 };
+
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+
+function MyButton() {
+  return (
+    <button className='red-btn'>I'm a button</button>
+  );
+}
 
 function Profile() {
   return (
@@ -47,13 +53,23 @@ if (true) {
 }
 
 function App() {
-
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {product.title}
+    </li>
+  )
   return (
     <>
       <h1 style={{ color: 'blue' }}>App Component</h1>
       <MyButton />
       <Profile />
       {content}
+      <ul>{listItems}</ul>
     </>
   )
 }
