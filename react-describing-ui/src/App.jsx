30135da0feacd8792eb1cec14f3c88a9b0ff1abb
aljon <aljonkenfernandez36@gmail.com>
2025-10-25 09:08:40 +0800

    Done with how to conditionally render components

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -1,13 +1,15 @@
 import Gallery from "../src/components/Gallery"
 import TodoList from "./components/TodoList"
 import Profile from "./components/Profile"
+import PackingList from "./components/PackagingList"
 function App() {
 
   return (
     <>
       {/* <Gallery /> */}
       {/* <TodoList /> */}
-      <Profile />
+      {/* <Profile /> */}
+      <PackingList />
     </>
   )
 }
