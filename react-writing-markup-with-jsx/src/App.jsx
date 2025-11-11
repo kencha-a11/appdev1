cf6efeec8d2c6f67d1eac92fe86cdce759e662f0
aljon <aljonkenfernandez36@gmail.com>
2025-11-11 12:54:24 +0800

    Done with the rules of JSX

--- react-writing-markup-with-jsx/src/App.jsx
+++ react-writing-markup-with-jsx/src/App.jsx
@@ -11,13 +11,13 @@ export default function App() {
 
 export function TodoList() {
   return (
-    <> {/* <--- must have one parent element to work */}
+    <> {/* <--- must have one parent element to work --- 1.) Return sinble root element (<></> or <div> and others) */}
       <h1>Hedy Lamarr's Todos</h1>
       <img
         src="https://i.imgur.com/yXOvdOSs.jpg"
         alt="Hedy Lamarr"
-        className="photo" // <--- HTML uses class reserve keyword JSX uses the className
-      /> {/* <--- img html element must be closed JSX strict rule */}
+        className="photo" // <--- HTML uses class reserve keyword JSX uses the className --- 3.) camelCase all most of the things CSS, HTML, JS etc
+      /> {/* <--- img html element is a self closing tag must be closed JSX strict rule --- 2.) Close all the tags*/}
 
       <ul>
         <li>Invent new traffic lights</li> {/* <--- all html element must be properly closed in JSX*/}
@@ -52,4 +52,16 @@ export function TodoList() {
  * 
  * Helpful Note:
  * React on-screen error messages will help solve problem
+ */
+
+/**
+ * The Rules of JSX
+ * 1.) Return a single root element (parent)
+ * 2.) Close all the HTML tags
+ * 3.) camelCase all most of the things
+ * 
+ * JSX tags must be wrap
+ * Reason:
+ * Its just Javascript behind transformed into plain javascript object
+ * Returning two object is prohibited unless wrap into array (fragment or element)
  */
