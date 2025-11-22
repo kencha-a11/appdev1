cf8bf0837d23cfda5831757a2f728d69fe35ab0e
aljon <aljonkenfernandez36@gmail.com>
2025-11-22 14:15:13 +0800

    Done with passing strings with quotes

---
+++ react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import Avatar from './components/Avatar'
+
+function App() {
+
+  return (
+    <>
+      <Avatar />
+    </>
+  )
+}
+
+export default App
+
+
---
+++ react-javascript-in-jsx-with-curly-braces/src/components/Avatar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Avatar() {
+    return (
+        <img
+            className="avatar"
+            src="https://i.imgur.com/7vQD0fPs.jpg"
+            alt="Gregorio Y. Zara"
+        />
+    )
+}
+
+// Passing Strings with quotes
+// one usage is html element attribute that uses strings to pass
