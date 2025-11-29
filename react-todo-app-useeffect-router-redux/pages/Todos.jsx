c1d9fa24a4fc519eaacd0d8d38ca737e1c01c931
aljon <aljonkenfernandez36@gmail.com>
2025-11-29 08:13:43 +0800

    Done with Create the Todos page section

--- react-todo-app-useeffect-router-redux/pages/Todos.jsx
+++ react-todo-app-useeffect-router-redux/pages/Todos.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { fetchTodos } from "../features/todos/todosSlice"
+import AddTodoForm from "../components/AddTodoForm"
+import TodoList from "../components/TodoList"
 
-export default function Todos() {
-  return (
-    <div>
-      Todos Page
-    </div>
-  )
+
+function Todos () {
+    const dispatch = useDispatch()
+    const todos = useSelector(state => state.todos.items)
+
+    useEffect(() => {
+        dispatch(fetchTodos())
+    }, [dispatch])
+
+    return (
+        <div>
+            <h1>Todos</h1>
+            <AddTodoForm />
+            <TodoList todos={todos} />
+        </div>
+    )
 }
+
+export default Todos
